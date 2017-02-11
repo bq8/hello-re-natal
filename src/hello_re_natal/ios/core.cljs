9bad2b6be7e196a73dbda49ea98db8d4407da3ac
Brian Quach <bquach88@gmail.com>
2017-02-11 00:37:40 -0500

    Add style file.

--- src/hello_re_natal/ios/core.cljs
+++ src/hello_re_natal/ios/core.cljs
@@ -3,7 +3,8 @@
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [hello-re-natal.events]
             [hello-re-natal.subs]
-            [cljs.reader :as reader]))
+            [cljs.reader :as reader]
+            [hello-re-natal.ios.styles :as s]))
 
 (def read-string reader/read-string)
 (def ReactNative (js/require "react-native"))
@@ -55,13 +56,20 @@
               (pos? (val x))) 
             (baz {} weight-set weight-per-side))))
 
+;;
+;;
+
 (defn app-root []
   (let [greeting (subscribe [:get-greeting])
         state (atom "")
-        desired-units (atom 0)]
+        desired-units (atom 0)
+        target-weight-unit (atom 0)
+        target-weight (atom 0)
+        disc-unit (atom 0)]
     (fn []
       [view {:style {:flex-direction "column"
-                     :margin 40}}
+                     :padding 40
+                     :background-color "#2176ae"}} ;; 56cbf9
        [text {:style {:font-size 30
                       :font-weight "100"
                       :margin-bottom 20
@@ -71,34 +79,38 @@
                        :width 80
                        :height 80
                        :margin-bottom 30}}]
-       [text-input {:style {:height 40}
-                    :keyboard-type "numeric"
-                    :on-change-text #(do
-                                       (reset! state %)
-                                       (r/flush))
-                    :placeholder "Weight"
-                    :return-key-type "done"
-                    :value @state}]
-       [segmented-control {:on-change #(reset! desired-units (-> % .-nativeEvent .-selectedSegmentIndex))
-                           :selected-index @desired-units
+       [text {:style (get-in s/styles [:text-label])} "How much weight do you want to lift?"]
+      [text-input {:style {:height 40
+                           :color "#ff729f"}
+                   :keyboard-type "numeric"
+                   :on-change-text #(do
+                                      (reset! state %)
+                                      (r/flush))
+                   :placeholder "Weight"
+                   :return-key-type "done"
+                   :value @state}]
+       [segmented-control {:on-change #(do 
+                                         (reset! target-weight-unit (-> % .-nativeEvent .-selectedSegmentIndex))
+                                         ;; fixme: reset target weight here
+                                         )
+                           :selected-index @target-weight-unit
+                           :tint-color (get-in s/styles [:segmented-control :tint-color])
                            :values ["kg", "lbs"]}]
-       [touchable-highlight {:style {:background-color "#999"
-                                     :padding 10
-                                     :border-radius 5}
-                             :on-press #(alert (str (to-pounds 135)))}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]
-       [view {:style {:background-color "#000"
-                      `:height 80}}]
-       [view {:style {:background-color "red"
-                      `:height 80}}]
+       [text {:style (get-in s/styles [:text-label])} "What kind of bar do you have?"]
+       [segmented-control {:tint-color (get-in s/styles [:segmented-control :tint-color])
+                           :values ["Mens 20kg/44lb Olympic", "Womens 15kg/33lb Olympic"]}]
+       [text {:style (get-in s/styles [:text-label])} "What kind of weights will you be using?"]
+       [segmented-control {:on-change #(reset! disc-unit (-> % .-nativeEvent .-selectedSegmentIndex))
+                           :tint-color (get-in s/styles [:segmented-control :tint-color])
+                           :values ["kilogram discs", "pound discs"]}]
        [text {:style {:color "black"
                       :text-align "center"
                       :font-weight "bold"}}
                       (if (number? (read-string @state))
                           (str (calc-weight @state 
-                                            (if (zero? @desired-units) 20 45) 
-                                            (if (zero? @desired-units) kilogram-plates pound-plates))) ;;(str (to-pounds @state))
-                          (str "Not a number" @state (rand 10));;
+                                            (if (zero? @disc-unit) 20 45) 
+                                            (if (zero? @disc-unit) kilogram-plates pound-plates)))
+                          (str "Not a number" @state (rand 10))
                           )]])))
 
 (defn init []
---
+++ src/hello_re_natal/ios/styles.cljs
@@ -0,0 +1,5 @@
+(ns hello-re-natal.ios.styles)
+
+(def styles
+  {:segmented-control {:tint-color "#fbb13c"}
+   :text-label {:color "#fe6847"}})
